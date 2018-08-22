812cd9e6d7ac27b53ff6a60ad9ad7d429a6f24e0
Thiago Butignon <thiagobutignon@gmail.com>
2018-08-22 19:37:24 -0300

    exercicio 1 resolvido

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<body>
+
+<h1>Aula 14 - PHP Loop</h1>
+<h2>Exercício 1</h2>
+    <?php 
+        for ($i = 0; $i <= 100; $i++) {
+            echo "<p>$i</p>";
+        }
+    ?>
+
+
+    
+</body>
+</html>
