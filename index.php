0a1302cd3d5e8e8f86dde4eaee59347d33795837
Thiago Butignon <thiagobutignon@gmail.com>
2018-08-22 19:42:53 -0300

    Exercicio 2 e 3 resolvidos

--- index.php
+++ index.php
@@ -10,6 +10,20 @@
             echo "<p>$i</p>";
         }
     ?>
+<h2>Exercício 2</h2>
+    <?php 
+        for ($i = 0; $i <= rand(1,100); $i++) {
+            echo "<p>$i</p>";
+        }
+    ?>
+
+<h2>Exercício 3</h2>
+    <?php
+        for ($j = 0; $j <= 10; $j++) {
+            $tabuadaDoDois = $j * 2;
+            echo "<p>$j x 2 = $tabuadaDoDois</p>";
+        }
+    ?>
 
 
     
